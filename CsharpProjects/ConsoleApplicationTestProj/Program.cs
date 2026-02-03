49bff3b7948f755933050529840e675484e9a75e
Joshua Morgan <JoshuaReubenMorgan@gmail.com>
2026-02-03 15:09:21 +0000

    Primarily doing the coinflip challenge for this commit. Was very basic warmup

---
+++ CsharpProjects/ConsoleApplicationTestProj/Program.cs
@@ -0,0 +1,38 @@
+﻿// Practice Section
+
+
+// string value1 = " a";
+// string value2 = "A ";
+
+// Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+// Console.WriteLine("a" != "a");
+// Console.WriteLine("a" != "A");
+// Console.WriteLine(1 != 2);
+
+// string myValue = "a";
+// Console.WriteLine(myValue != "a");
+
+
+// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+
+// string pangram = "The quick brown fox jumps over the lazy dog.";
+
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+//////////////////////////////////////
+
+// int saleAmount = 1001;
+// int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+
+Random rand = new();
+int[] coin = [1, 2];
+int result = rand.Next(coin.Length);
+Console.WriteLine($"Coin Flip Result: {(result == 1 ? "Heads" : "Tails")}");
+
